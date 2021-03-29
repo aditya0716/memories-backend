30709213a0f3b6410c19d38fcd8305239e02849a
aditya0716 <aditya.shandilya16798@gmail.com>
2021-03-29 20:44:53 +0530

    memories

--- index.js
+++ index.js
@@ -30,6 +30,10 @@ app.use(cors());
 app.use("/user", userRoutes);
 app.use("/posts", postRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Hello, My name is Aditya");
+});
+
 // post
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server started at PORT", PORT));
